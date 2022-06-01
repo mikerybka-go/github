bf539a742f66336f3b1fe7601b53c2c6f4bf2fe0
Michael Rybka <mikerybka@users.noreply.github.com>
2022-05-31 20:09:44 -0400

    Update add_headers.go

--- add_headers.go
+++ add_headers.go
@@ -2,7 +2,7 @@ package github
 
 import "net/http"
 
-func addHeaders(r *http.Request, token string) {
+func addHeaders(r *http.Request, userID, token string) {
 	r.Header.Set("Authorization", "token "+token)
 	r.Header.Set("Accept", "application/vnd.github.v3+json")
 }
